af1db740fda36adc8c187d6b4e08cfca99f56f9f
oscar <oclemente.dscont@gmail.com>
2023-08-13 22:29:20 -0500

    cambios1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
-//import { Button } from "@mui/material";
+import  { useState } from 'react';
+
+// import { Container } from "@mui/material";
+import { TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import Button  from "@mui/material/Button";
 
 export default function App(){
+
+  const [searchText, setSearchText] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const handleSearch = () => {
+    // Aquí puedes implementar la lógica para realizar la búsqueda
+  };
+  // Datos de ejemplo para la tabla
+  const tableData = [
+    { nro: 1, cantidad: 10, codigo: 'A001', descripcion: 'Producto 1' },
+    { nro: 2, cantidad: 5, codigo: 'A002', descripcion: 'Producto 2' },
+    // ...
+  ];
   return (
-    <>
-      <h1>App</h1>
-      <Button variant="contained">Mi primer Boton</Button>
-    </>
+    <div>
+      <div>
+        <TextField
+          label="Buscar"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleSearch}>Buscar</Button>
+        <TextField
+          label="Cantidad"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+      </div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nro</TableCell>
+              <TableCell>Cantidad</TableCell>
+              <TableCell>Código</TableCell>
+              <TableCell>Descripción</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData.map((row) => (
+              <TableRow key={row.nro}>
+                <TableCell>{row.nro}</TableCell>
+                <TableCell>{row.cantidad}</TableCell>
+                <TableCell>{row.codigo}</TableCell>
+                <TableCell>{row.descripcion}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );  
 }
