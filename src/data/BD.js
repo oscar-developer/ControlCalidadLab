b9b83827f57d5f2a2015a36c98b2234342ba117a
oscar <oclemente.dscont@gmail.com>
2023-08-19 19:36:09 -0500

    dsd

--- src/data/BD.js
+++ src/data/BD.js
@@ -8,18 +8,30 @@ const BDJson = {
         },
         {
             id:2,
-            cantidad:9,
-            unidad_medida:'Tubos',
-            denominacion:'Lauril Simple',
+            cantidad:1,
+            unidad_medida:'frasco',
+            denominacion:'APT',
         },
         {
             id:3,
-            cantidad:90,
-            unidad_medida:'Ml',
-            denominacion:'Ogye',
+            cantidad:1.5,
+            unidad_medida:'frasco',
+            denominacion:'Baird Parker',
         },
         {
             id:4,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },
+        {
+            id:5,
+            cantidad:9,
+            unidad_medida:'Tubos',
+            denominacion:'Lauril Simple',
+        },        
+        {
+            id:6,
             cantidad:2,
             unidad_medida:'Tubos',
             denominacion:'AP',
@@ -44,7 +56,372 @@ const BDJson = {
             unidad_medida:'Frasco',
             denominacion:'Baird Parker',
         }
-    ]
+    ],
+    "Harina AOAC":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'Solucion bufferada (SB)',
+        },
+        {
+            id:2,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'Agua Peptonada Tamponada(APT)',
+        },
+        {
+            id:3,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'agua peptonada (AP)',
+        },
+        {
+            id:4,
+            cantidad:1.5,
+            unidad_medida:'Frasco',
+            denominacion:'Baird Parker',
+        },
+        {
+            id:5,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'MYP',
+        }
+    ],
+    "Harina ICMS":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'Agua Peptonada (AP)',
+        },
+        {
+            id:2,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'Agua Peptonada Tamponada(APT)',
+        },
+        {
+            id:3,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Ogye',
+        },
+        {
+            id:4,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },
+        {
+            id:5,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'MYP',
+        },
+        {
+            id:6,
+            cantidad:9,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril simple',
+        },
+        {
+            id:7,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'Agua Peptonada (AP)',
+        },
+        {
+            id:8,
+            cantidad:1.5,
+            unidad_medida:'Frasco',
+            denominacion:'Baird Parker',
+        },
+    ],  
+    
+    "Hojuelas AOAC":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'SB',
+        },
+        {
+            id:2,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'APT',
+        },
+        {
+            id:3,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },        
+        {
+            id:4,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'MYP',
+        },        
+    ],
+    "Hojuelas ICMSF":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'AP',
+        },
+        {
+            id:2,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'APT',
+        },
+        {
+            id:3,
+            cantidad:90,
+            unidad_medida:'ML',
+            denominacion:'Ogye',
+        },        
+        {
+            id:4,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },
+        {
+            id:5,
+            cantidad:90,
+            unidad_medida:'ML',
+            denominacion:'MYP',
+        },        
+        {
+            id:6,
+            cantidad:9,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:7,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },                                
+    ],
+    "Cafe ICMSF":[
+        {
+            id:1,
+            cantidad:9,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:2,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },
+        {
+            id:3,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Ogye',
+        },        
+        {
+            id:4,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'AP',
+        },                
+    ],
+    "Chocolates ICMSF":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'AP',
+        },
+        {
+            id:2,
+            cantidad:9,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:3,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },        
+        {
+            id:4,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'APT',
+        },                
+    ],
+    "Yogurt ICMS":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'AP',
+        },
+        {
+            id:2,
+            cantidad:9,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:3,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Ogye',
+        },        
+        {
+            id:4,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },                
+    ],
+    "Leche ICMSF":[
+        {
+            id:1,
+            cantidad:12,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:2,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },
+        {
+            id:3,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },    
+    ],
+    "Quesos ICMSF":[
+        {
+            id:1,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'AP',
+        },
+        {
+            id:2,
+            cantidad:12,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },
+        {
+            id:3,
+            cantidad:2,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },        
+        {
+            id:4,
+            cantidad:120,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },
+        {
+            id:5,
+            cantidad:3,
+            unidad_medida:'Frasco',
+            denominacion:'Baird Parker',
+        },                                
+    ],                                                                                                
+    "Agua Potable":[
+        {
+            id:1,
+            cantidad:10,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril doble',
+        },
+        {
+            id:2,
+            cantidad:60,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },        
+    ],
+    "Agua Manante":[
+        {
+            id:1,
+            cantidad:5,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril doble',
+        },
+        {
+            id:2,
+            cantidad:10,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril Simple',
+        },        
+    ],    
+    "Agua Residual":[
+        {
+            id:1,
+            cantidad:5,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril doble',
+        },
+        {
+            id:2,
+            cantidad:40,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril simple',
+        },
+        {
+            id:3,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'APa',
+        },
+        {
+            id:4,
+            cantidad:4,
+            unidad_medida:'Tubo',
+            denominacion:'AP',
+        },
+    ],
+    "Agua Superficial":[
+        {
+            id:1,
+            cantidad:5,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril doble',
+        },
+        {
+            id:2,
+            cantidad:10,
+            unidad_medida:'Tubo',
+            denominacion:'Lauril simple',
+        },
+        {
+            id:3,
+            cantidad:1,
+            unidad_medida:'Frasco',
+            denominacion:'APa',
+        },
+        {
+            id:4,
+            cantidad:90,
+            unidad_medida:'ml',
+            denominacion:'Plate count',
+        },
+     ],   
+
+
 
 }
 
